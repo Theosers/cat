0b4ed3be0b838f8bbe704ef337cb81404caee56d
Theosers <theophile.sers@gmail.com>
2023-10-25 17:49:53 +0200

    CatBoard design fini

--- catBoard.js
+++ catBoard.js
@@ -1,7 +1,10 @@
-// Crée le div principal attaché au body
+// Crée le div principal & titre attaché au body
 const body = document.querySelector('body');
 const catBoard = document.createElement('div');
 catBoard.setAttribute('id', 'catBoard');
+const titre = document.createElement('h1');
+titre.setAttribute('id', 'titre');
+titre.textContent = 'Catastrophe';
 
 // Crée l'élément <select> pour choisir la race de chat
 const choixRaceSelect = document.createElement('select');
@@ -11,6 +14,10 @@ choixRaceSelect.onchange = function () {
     console.log('fonction sur le onchange');
     // showBreedImage(this.value);
 };
+
+// crée le div picture = infosLeft + image + infosRight
+const divPicture = document.createElement('div');
+divPicture.setAttribute('id', 'divPicture');
 
 //Crée l'élément div pour afficher l'image du chat
 const divImage = document.createElement('div');
@@ -22,16 +29,93 @@ image.setAttribute('id', 'image');
 divImage.appendChild(image);
 
 
+//Crée l'élément div pour afficher les infos(css) du chat 
+//GAUCHE => Amour
+
+const divInfosLeft = document.createElement('div');
+divInfosLeft.setAttribute('id', 'divInfosLeft');
+
+const affection_level = document.createElement('p'); // coeur
+affection_level.setAttribute('id', 'affection_level');
+const child_friendly = document.createElement('p'); // enfant + coeur
+child_friendly.setAttribute('id', 'child_friendly');
+const dog_friendly = document.createElement('p'); // chien + coeur
+dog_friendly.setAttribute('id', 'dog_friendly');
+
+divInfosLeft.appendChild(affection_level);
+divInfosLeft.appendChild(child_friendly);
+divInfosLeft.appendChild(dog_friendly);
+//Crée l'élément div pour afficher les infos(css) du chat 
+//DROITE => Valeurs
+
+const divInfosRight = document.createElement('div');
+divInfosRight.setAttribute('id', 'divInfosRight');
+
+const energy_level = document.createElement('p'); // eclair
+energy_level.setAttribute('id', 'energy_level');
+const intelligence = document.createElement('p'); // cerveau
+intelligence.setAttribute('id', 'intelligence');
+const rare = document.createElement('p'); // diamant
+rare.setAttribute('id', 'rare');
+
+divInfosRight.appendChild(energy_level);
+divInfosRight.appendChild(intelligence);
+divInfosRight.appendChild(rare);
+
+divPicture.appendChild(divInfosLeft);
+divPicture.appendChild(divImage);
+divPicture.appendChild(divInfosRight);
+
 //Crée l'élément div pour afficher la description du chat
 
-//Crée l'élément div pour afficher les infos(css) du chat
+const divDescription = document.createElement('div');
+divDescription.setAttribute('id', 'divDescription');
+
+const description = document.createElement('p');
+description.setAttribute('id', 'description');
+
+const origin = document.createElement('p');
+origin.setAttribute('id', 'origin');
+
+const name = document.createElement('p');
+name.setAttribute('id', 'name');
+
+const life_span = document.createElement('p');
+life_span.setAttribute('id', 'life_span');
+
+const temperament = document.createElement('p');
+temperament.setAttribute('id', 'temperament');
+
+const divDescriptionEntete = document.createElement('div');
+divDescriptionEntete.setAttribute('id', 'divDescriptionEntete');
+
+const divDescriptionBody = document.createElement('div');
+divDescriptionBody.setAttribute('id', 'divDescriptionBody');
+
+divDescriptionEntete.appendChild(name);
+divDescriptionEntete.appendChild(origin);
+divDescriptionEntete.appendChild(life_span);
+
+divDescriptionBody.appendChild(temperament);
+divDescriptionBody.appendChild(description);
+
+divDescription.appendChild(divDescriptionEntete);
+divDescription.appendChild(divDescriptionBody);
+
+
+
 
 // Ajoute le sélecteur au corps de la page
-catBoard.appendChild(choixRaceSelect);
-catBoard.appendChild(divImage);
-
-
+body.appendChild(titre);
 body.appendChild(catBoard);
+
+catBoard.appendChild(choixRaceSelect);
+catBoard.appendChild(divPicture);
+
+catBoard.appendChild(divDescription);
+
+
+
 
 export const getblabla = () => {
     
--- index.js
+++ index.js
@@ -18,17 +18,47 @@ const fillSelectOptions = (races) => {
 
 }
 const fillBoardByDefault = (races) => {
+    //fill image
     const divImage = document.getElementById('divImage');
     const image = document.getElementById('image');
     image.src = races[0].image.url;
     image.alt = `chat de la race ${races[0].name}`;
     console.log(races[0].image.width / races[0].image.height);
     let imageRatio = races[0].image.width / races[0].image.height;
-
     divImage.style.width = `${imageRatio*300}px`;
     divImage.style.height = `${imageRatio*300}px`;
     image.style.width = `${imageRatio*300}px`;
     image.style.height = `${imageRatio*300}px`;
+
+    //fill description
+    const description = document.getElementById('description');
+    const origin = document.getElementById('origin');
+    const name = document.getElementById('name');
+    const life_span = document.getElementById('life_span');
+    const temperament = document.getElementById('temperament');
+
+    description.innerText = races[0].description;
+    origin.innerText = `Origin : ${races[0].origin}`;
+    name.innerText = `Breed : ${races[0].name}`;
+    life_span.innerText = `Life span : ${races[0].life_span}`;
+    temperament.innerText = `${races[0].temperament}`;
+
+    //fill infos left
+
+    const affection_level = document.getElementById('affection_level');
+    const child_friendly = document.getElementById('child_friendly');
+    const dog_friendly = document.getElementById('dog_friendly');
+    affection_level.innerText = `Affection level : ${races[0].affection_level}` ;
+    child_friendly.innerText = `Child Friendly : ${races[0].child_friendly}` ;
+    dog_friendly.innerText = `Dog Friendly : ${races[0].dog_friendly}` ;
+
+    //fill infos right
+    const energy_level = document.getElementById('energy_level');
+    const intelligence = document.getElementById('intelligence');
+    const rare = document.getElementById('rare');
+    energy_level.innerText = `Energy : ${races[0].energy_level}` ;
+    intelligence.innerText = `Intelligence : ${races[0].intelligence}` ;
+    rare.innerText = `Rarity : ${races[0].rare}`;
 }
 
 fetch('https://api.thecatapi.com/v1/breeds',
@@ -45,6 +75,7 @@ fetch('https://api.thecatapi.com/v1/breeds',
    let races = data;
    fillSelectOptions(races);
    fillBoardByDefault(races);
+
     
     })
     .catch(err => console.log('Une erreur est survenue'));
